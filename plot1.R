02a57a4dae86be74eb419a0c919d6877de09c502
Hugo Day <hugoaday@gmail.com>
2018-01-27 22:26:43 +0000

    First commit

---
+++ plot1.R
@@ -0,0 +1,23 @@
+# This code is for exploratory data analysis assignment 1
+
+datRaw = read.table("F:/ExData_Plotting1/household_power_consumption.txt",header=TRUE,sep=";",dec=".");
+colnames(datRaw) <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage",
+                      "Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3");
+datRaw<-na.omit(datRaw)
+datRaw$Date <- as.Date(datRaw$Date,"%d/%m/%Y");
+datRaw$Time <- as.POSIXlt(datRaw$Time,"%H:%M:%S")
+datRaw$Global_active_power<-as.numeric(as.character(datRaw$Global_active_power))
+datRaw$Global_reactive_power<-as.numeric(as.character(datRaw$Global_reactive_power))
+datRaw$Voltage<-as.numeric(as.character(datRaw$Voltage))
+datRaw$Global_intensity<-as.numeric(as.character(datRaw$Global_intensity))
+datRaw$Sub_metering_1<-as.numeric(as.character(datRaw$Sub_metering_1))
+datRaw$Sub_metering_2<-as.numeric(as.character(datRaw$Sub_metering_2))
+datRaw$Sub_metering_3<-as.numeric(as.character(datRaw$Sub_metering_3))
+datMin <- as.Date("01-02-2007","%d-%m-%Y");
+datMax <- as.Date("02-02-2007","%d-%m-%Y");
+datLim <- datRaw[(datRaw$Date>=datMin) & (datRaw$Date<=datMax),];
+
+png(filename = "F:/ExData_Plotting1/plot1.png",width = 480, height = 480)
+hist(datLim$Global_active_power,breaks=16,xlab="Global Active Power (kilowatts)", 
+     ylab="Frequency", main="Global Active Power",col="red")
+dev.off()
